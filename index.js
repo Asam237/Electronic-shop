7c787be16bee7cd2e20008d1637391f727b15515
Asam237 <abbasalimokolo@gmail.com>
2021-11-22 06:12:47 +0100

    update server file

--- index.js
+++
@@ -1,2 +0,0 @@
-require("./mongos");
-require("./server");
--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const port = process.env.PORT || 5000
-const users = require("./routes/api/users")
-const profil = require("./routes/api/profiles")
-const passport = require("passport");
-require("./config/passport")(passport)
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const PORT = process.env.PORT || 5000
+const AuthRoutes = require("./routes/auth")
+const mongoose = require("mongoose")
+const url = "mongodb://localhost:27017/MyTest";
+const passport = require("passport")
 
+const options = {
+    promiseLibrary: Promise,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}
 
-app.use(bodyParser.json())
+mongoose.connect(url, options);
+mongoose.connection.on("connected", () => console.log("[MongoDB] is running on 5000"))
+mongoose.connection.on("disconnected", () => console.log("[MongoDB] is not running"))
+
 app.use(passport.initialize())
-app.use("/api/users", users)
-app.use("/api/profile", profil)
-
-app.listen(port, () => console.log(`[ExpressJS] is running on ${port}`))
+require("./config/passport")(passport)
+app.get("/", (req, res) => res.send("Hello !"))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(AuthRoutes)
+app.listen(PORT, () => console.log(`[Express JS] is running on ${PORT}`))

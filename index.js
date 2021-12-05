b5092f955c5eb71c2f0d7b58934c52c12a9a1bd9
Asam237 <abbasalimokolo@gmail.com>
2021-12-05 06:29:02 +0100

    express index

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const authRoute = require("./routes/auth")
+const categoryRoute = require("./routes/category")
+const productRoute = require("./routes/product")
+
+dotenv.config()
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB connection successfull"))
+    .catch(e => console.log(e))
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({
+
+    extended: true
+
+}));
+app.use("/api/auth", authRoute)
+app.use("/api/categories", categoryRoute)
+app.use("/api/products", productRoute)
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("BackEnd is running !")
+})
